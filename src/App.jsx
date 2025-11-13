381e1c2eeaa9f2d375a075126dc4ae98492c9d90
Camilo <camiloolave22@gmail.com>
2025-11-12 19:06:15 -0500

    pagina cambio de contraseña funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import LibroForm from "./components/LibroForm";
-import LibroList from "./components/LibroList";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
+import ChangePassword from "./pages/ChangePassword/ChangePassword";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/Login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/change-password" element={<ChangePassword />} />
       </Routes>
     </BrowserRouter>
   );
